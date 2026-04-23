f8505737774043a2c1e03066f9c556ce93fe8a7e
Polarnik <rostislav.voloshin@yandex.ru>
2026-04-23 11:50:32 +0700

    refactor: Сделан CodeGenerator независимым от бэкенда

--- src/components/VisualEditor/generators/index.ts
+++ src/components/VisualEditor/generators/index.ts
@@ -1,10 +1,1 @@
 export { CodeGenerator } from './CodeGenerator';
-export { luaBackend } from './luaBackend';
-export { 
-  type TraverseFn, 
-  type GeneratorContext, 
-  type NodeGenerator, 
-  type GeneratorBackend,
-  type NodeExecuteGenerator,
-  type NodeEvaluateGenerator
-} from './types';
