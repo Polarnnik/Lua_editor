de937988a90aefeda67d496b95a34aa0672782cc
Polarnik <rostislav.voloshin@yandex.ru>
2026-04-29 13:09:56 +0700

    feat: публичное API библиотеки

---
+++ src/lib/index.ts
@@ -0,0 +1,42 @@
+export { default as VisualEditor } from "./VisualEditor";
+export type { VisualEditorProps, VisualEditorHandle } from "./VisualEditor";
+
+export type {
+  NodeDefinition,
+  NodeCodegen,
+  NodeCodegenExecute,
+  NodeCodegenEvaluate,
+  PinDef,
+  PinType,
+  GeneratorContext,
+  TraverseFn,
+  CodeBackend,
+  EditorTheme,
+} from "./types";
+export { defaultTheme, PIN_COLORS } from "./types";
+
+export { builtinNodes } from "./builtins/index";
+
+export { luaBackend } from "./backends/lua";
+
+export {
+  id,
+  literal,
+  binaryOp,
+  unaryOp,
+  call,
+  exprStmt,
+  assign,
+  localDecl,
+  ifStmt,
+  whileStmt,
+  forStmt,
+  returnStmt,
+  funcDecl,
+  program,
+  mathCall,
+} from "./ast/builders";
+export type { Expr, Stmt, Program } from "./ast/types";
+
+export { BaseNode } from "./nodes/BaseNode";
+export type { BaseNodeProps, Pin } from "./nodes/BaseNode";
